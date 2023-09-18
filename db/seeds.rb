1289cba84ed2c2ffd63556cefe64f43bd14fede1
inoulen inoulen <inoulenkon@gmail.com>
2023-09-18 22:56:23 +0530

    Seed some default values and install react-redux, reduxjs/toolkit and axios libraries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting: 'Hello')
+Greeting.create(greeting: 'Good Day')
+Greeting.create(greeting: 'Good Morning')
+Greeting.create(greeting: 'Good Night')
+Greeting.create(greeting: 'Howdy')
